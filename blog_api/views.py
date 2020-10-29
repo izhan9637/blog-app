6043d9dbbb31edff368d162c67e3442c23f20228
izhan9637 <izhan9637@gmail.com>
2020-10-29 16:28:27 +0530

    Added project level and view level permissions

--- blog_api/views.py
+++ blog_api/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, DjangoModelPermissions, BasePermission, IsAuthenticatedOrReadOnly
+#DjangoModelPermissionsOrAnonReadOnly, IsAdminUser
+
+
+class PostUserWritePermission(BasePermission):
+    message = "Editing post is restricted to author only"
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer 
